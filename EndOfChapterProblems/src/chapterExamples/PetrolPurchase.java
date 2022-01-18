794389b3640093edc55838f2853b457b5d61e093
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-01-18 14:42:03 -0600

    Completed Chapter 3 exercise 12 Petrol&PetrolPurchase, updated README to reflect the changes.

---
+++ EndOfChapterProblems/src/chapterExamples/PetrolPurchase.java
@@ -0,0 +1,60 @@
+package chapterExamples;
+
+public class PetrolPurchase {
+	
+	// initializing Instance Variables with specified types
+	private String location;
+	private String petrol;
+	private int quantity;
+	private double price;
+	private double discount;
+	private double netPurchaseAmount;
+	
+	// Constructor object initializing the five instance variables
+	public PetrolPurchase(String location, String petrol, int quantity, double price, double discount) {
+		this.location = location;
+		this.petrol = petrol;
+		this.quantity = quantity;
+		this.price = price;
+		this.discount = discount;
+	}
+	
+	public void setLocation(String location){
+		this.location = location;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setPetrol(String petrol) {
+		this.petrol = petrol;
+	}
+	public String getPetrol() {
+		return petrol;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setNetPurchase(double netPurchaseAmount) {
+		this.netPurchaseAmount = netPurchaseAmount;
+	}
+	public double getNetPurchase() {
+		return netPurchaseAmount = ((double) (price - (price * discount)) * quantity);
+	}
+	
+}
