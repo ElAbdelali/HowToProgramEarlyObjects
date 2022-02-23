b2eb25a8018c6e24712a072d919d7f5af5394287
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-02-22 21:06:44 -0600

    Completed Clock Assignment

---
+++ EndOfChapterProblems/src/chapterExamples/ClockTest.java
@@ -0,0 +1,15 @@
+package chapterExamples;
+
+public class ClockTest {
+	
+	
+	public static void main(String[] args) {
+		Clock newClock = new Clock(24, 3, 9);
+		
+		newClock.setHour(newClock.getHour());
+		newClock.setMinute(newClock.getMinute());
+		newClock.setSecond(newClock.getSecond());
+		
+	}
+
+}
