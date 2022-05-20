062cb89a676a2354d2c8341c9e40378430fee552
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-05-20 00:06:16 -0500

    Started Duplicate Elimation exercise

---
+++ EndOfChapterProblems/src/chapterExamples/DuplicateElimination.java
@@ -0,0 +1,30 @@
+package chapterExamples;
+import java.util.Arrays;
+import java.util.Scanner;
+public class DuplicateElimination {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int[] array = new int[10];
+		
+		Arrays.fill(array, -1);
+		
+		for(int value = 0; value < array.length; value++) {
+			System.out.printf("%d	%d%n", value, array[value]);
+		}
+		for(int inputValue = 0; inputValue < 10; inputValue++) {
+			System.out.print("Please enter a value between 10-100:");
+			array[inputValue] = input.nextInt();
+			//if(input.nextInt() < 10 || input.nextInt() > 100) {
+				//System.out.print("Please enter a valid value between 10-100: ");
+				//array[inputValue] = input.nextInt();
+			//}
+
+		}
+		for(int value = 0; value < array.length; value++) {
+			System.out.printf("%d	%d%n", value, array[value]);
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -152,4 +152,8 @@ Use a one-dimensional array to solve the following problem: You want to eventual
 	i) For 161–180: 170, and for all other pixel values, assumed a quantized value of 190
 	Print the array with the new, quantized values.
 
+7.12 (Duplicate Elimination) 
+
+Use a one-dimensional array to solve the following problem: Write an application that inputs ten numbers, each between 10 and 100, both inclusive. Save each number that was read in an array that was initialized to a value of -1 for all elements. Assume a value of -1 indicates an array element is empty. You are then to process the array, and remove duplicate elements from the array containing the numbers you input. Display the contents of the array to demonstrate that the duplicate input values were actually removed. [Note: do not display the array elements where the value is -1.]
+
 
