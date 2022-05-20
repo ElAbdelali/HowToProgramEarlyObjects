1211cba630f785fa701b24b89197b1551e63756d
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-05-19 23:16:33 -0500

    Completed the Pixel Quantization assignment

--- EndOfChapterProblems/src/chapterExamples/PixelQuantization.java
+++ EndOfChapterProblems/src/chapterExamples/PixelQuantization.java
@@ -6,19 +6,19 @@ public class PixelQuantization {
 		int startingPixel = 10;
 		int additionalPixels = 20;
 		
-		int[] pixelQuantization = new int[200];
+		int[] pixelQuantization = new int[201];
 		
-		for(int index = 0; index < pixelQuantization.length; index++) {
+		for(int index = 1; index < pixelQuantization.length; index++) {
 			pixelQuantization[index] = startingPixel;
 
-			if((index + 1) % 20 == 0) {
+			if((index) % 20 == 0) {
 				startingPixel+=additionalPixels;
 			}
 		}
 		
 		System.out.printf("The values of the array are:%n");
 		for(int value = 0; value < pixelQuantization.length; value++) { 
-		System.out.printf("Index:%d     Array Value:%d%n ", value+1, pixelQuantization[value]);
+		System.out.printf("Index:%d     Array Value:%d%n ", value, pixelQuantization[value]);
 		}
 		
 	}
