2fed9f1581e9f2bbf8dfeb224ca6956fee4c70a0
ElAbdelali <eljaouhariabdelali@outlook.com>
2022-07-24 19:03:25 -0500

    Completed chapter 14 exercise 4

--- EndOfChapterProblems/chapter14/chapter14exercise4/RegionMatches.java
+++ EndOfChapterProblems/chapter14/chapter14exercise4/RegionMatches.java
@@ -1,5 +1,12 @@
 package chapter14exercise4;
-
+/*
+ * 14.4 (Comparing Portions of Strings) Write an application that uses String method region-Matches to compare 
+ * two strings input by the user. 
+ * The application should input the number of characters to be compared and the starting index of the comparison. 
+ * The application should state
+ * whether the compared characters are equal. Ignore the case of the characters when performing the
+ * comparison.
+ */
 import java.util.Scanner;
 
 public class RegionMatches {
@@ -12,6 +19,11 @@ public class RegionMatches {
 		System.out.print("String 2: ");
 		String s2 = input.nextLine();
 
+		if(s1.regionMatches(true, 0, s2, 0, s1.length())){
+			System.out.printf("s1 matches s2");
+		}else {
+			System.out.printf("s1 does not match s2");
+		}
 
 	}
 }
